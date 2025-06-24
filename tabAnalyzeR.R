53d73188f4a63d3c7194c02e9bee7c9671d20d94
Alex Bokov <bokov@uthscsa.edu>
2025-06-24 11:06:10 -0500

    tabAnalyzeR.R; feature; Adds new script for analyzing open Firefox tabs

---
+++ tabAnalyzeR.R
@@ -0,0 +1,41 @@
+# Before running this script, make sure the lz4json package is installed
+library(jsonlite);
+library(tibble);
+library(dplyr);
+
+# init ----
+source('default_config.R');
+if(file.exists('local_config.R')) source('local_config.R');
+jsontemp <- tempfile('opentabs',fileext = '.json');
+
+# load data ----
+system(sprintf("lz4jsoncat %s > %s"
+               ,file.path(firefoxsessionpath,'sessionstore-backups','recovery.jsonlz4')
+               ,jsontemp),wait = T);
+
+opentabs <- fromJSON(jsontemp,simplifyVector = F);
+
+# parse data ----
+opentabslist <- list();
+kkrow <- iiwindow <- 0;
+for(ii in opentabs$windows){
+  iiwindow <- 1 + iiwindow;
+  jjtab <- 0;
+  for(jj in ii$tabs){
+    jjtab <- 1 + jjtab;
+    nthinhistory <- length(jj$entries);
+    kk <- jj$entries[[nthinhistory]];
+    kkrow <- 1 + kkrow;
+    opentabslist[[kkrow]] <- tibble(url=kk$url,title=kk$title,nthinhistory,lastAccessed=jj$lastAccessed,tab=jjtab,window=iiwindow);
+  }
+};
+
+opentabstable <- bind_rows(opentabslist) %>%
+  mutate(domain=gsub('^[a-z]*://([^/]*)/.*','\\1',url)
+         ,parent_domain=gsub('.*\\.([^.]*\\.[^.]*$)','\\1',domain));
+
+# analyze data ----
+
+by_domain <- summarise(opentabstable,tabs=n(),.by=domain) %>% arrange(desc(tabs));
+by_parent_domain <- summarise(opentabstable,tabs=n(),.by=parent_domain) %>% arrange(desc(tabs));
+
